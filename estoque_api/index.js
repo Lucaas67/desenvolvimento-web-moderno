c42aee500d64577c8a50c7e3ac93befe3dcf745f
Lucaas67 <lucasnevespereira@hotmail.com>
2024-08-27 11:03:16 -0300

    adicionando html

--- estoque_api/index.js
+++ estoque_api/index.js
@@ -1,22 +1,32 @@
+/* Importa o pacote express */ 
 const express = require('express');
+
+/* Importa o pacote mustache-express para renderizar HTML */ 
+const mustacheExpress = require('mustache-express');
+
 const db = require('./src/db');
 const estoqueRouter = require('./src/routers/estoqueRouter');
-const app = express();
-const PORT = 8080;
+const estoqueViewRouter = require('./src/routers/estoqueViewRouter');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); 
+/** Cria um aplicativo a partir do construtor do express */
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/src/views');
+
+app.use(express.urlencoded({extended: true}));
 
 app.use('/', estoqueRouter);
+app.use('/', estoqueViewRouter);
 
-db.sync()
-    .then(() => {
-        console.log('Banco de dados sincronizado');
-    })
-    .catch((err) => {
-        console.error('Erro ao sincronizar o banco de dados:', err);
-    });
+/** Sincroniza a conexão com o banco de dados. */
+db.sync(()=>{
+    console.log('DB conectado.');
+});
 
-app.listen(PORT, () => {
-    console.log(`Aplicativo web em execução na porta ${PORT}`);
+/** Declara uma porta e inicializa a execução do aplicativo na porta declarada */
+const PORT = 8080;
+app.listen(PORT, ()=>{
+    console.log('app rodando na porta ' + PORT);
 });
